14a6623e32086d6659eb2a5266161b828dcd05f4
Oliver Kabi <Oliver Kabi>
2016-08-15 20:56:40 +0100

    Initial commit

---
+++ Checkers.Tests/GivenACheckersBoard.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Checkers.Tests
+{
+    [TestFixture]
+    public class GivenACheckersBoard
+    {
+        private Board _subject;
+
+        [SetUp]
+        public void WhenAPieceIsMoved()
+        {
+            _subject = new Board();
+
+            var origin = new TileLocation { X = 0, Y = 0 };
+            var destination = new TileLocation { X = 1, Y = 1 };
+
+            _subject.MovePiece(origin, destination);
+        }
+
+        [Test]
+        public void ThenTheDestinationIsNowOccupied()
+        {
+            
+            Assert.That(_subject.Tiles[1, 1].IsOccupied, Is.True);
+        }
+
+        [Test]
+        public void ThenTheOriginIsNowEmpty()
+        {
+            Assert.That(_subject.Tiles[0, 0].IsOccupied, Is.False);
+        }
+    }
+}
---
+++ Checkers/Board.cs
@@ -0,0 +1,38 @@
+﻿namespace Checkers
+{
+    public class Board
+    {
+        public Tile[,] Tiles { get; set; }
+
+        public Board()
+        {
+            Tiles = new Tile[8,8];
+
+            for (var i = 0; i < Tiles.GetLength(0); i++)
+            {
+                for (var j = 0; j < Tiles.GetLength(1); j++)
+                {
+                    Tiles[i,j] = new Tile();
+                }
+            }
+        }
+
+        public void MovePiece(TileLocation origin, TileLocation destination)
+        {
+            Tiles[origin.X, origin.Y].IsOccupied = false;
+            Tiles[destination.X, destination.Y].IsOccupied = true;
+        }
+    }
+
+    public class TileLocation
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+
+    public class Tile
+    {
+        public bool IsOccupied { get; set; }
+    }
+}
